e123460e3af09fb0fedbe4c5df511f2235350227
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2023-09-28 14:07:55 +0530

    Leetcode 509. Fibonacci Number.js

--- Leetcode 509. Fibonacci Number.js
+++ Leetcode 509. Fibonacci Number.js
@@ -1,7 +1,16 @@
+// Loop Approach
 var fib = function(n) {
     const nums=[0,1];
     for(let i=2;i<=n;i++){
         nums.push(nums[i-1]+nums[i-2])
     }
     return nums[n];
+};
+
+// Recurssive Approach
+var fib = function(n) {
+   if(n<=1){
+       return n;
+   }
+   return fib(n-1)+fib(n-2);
 };
