b9a01314dbfe5a37807a7be21963e7c53e0d4bf4
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-06-17 17:47:15 +0530

    Create Leetcode 79. Word Search.cpp

---
+++ Leetcode 79. Word Search.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool solve(int i, int j, int n, int m,vector<vector<char>>& board, string word, int k){
+        if(k==word.size()){
+            return true;
+        }
+        if(i<0 ||j<0 || i>=n || j>=m || board[i][j]!=word[k]){
+            return false;
+        }
+
+        char ch=board[i][j];
+        board[i][j]='#';
+
+        //left
+        bool op1 =solve(i+1,j,n,m,board,word,k+1);
+        //right
+        bool op2=solve(i-1,j,n,m,board,word,k+1);
+        //up
+        bool op3=solve(i,j+1,n,m,board,word,k+1);
+        //down
+        bool op4=solve(i,j-1,n,m,board,word,k+1);
+
+        //backtraking
+        board[i][j]=ch;
+
+        return op1 || op2 || op3 || op4;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
+                    if(solve(i,j,n,m,board,word,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
