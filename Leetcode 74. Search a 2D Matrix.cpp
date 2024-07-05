6b6352c5160c8ea38f9fda2fb642f857bb908103
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-07-05 17:50:12 +0530

    Create Leetcode 74. Search a 2D Matrix.cpp

---
+++ Leetcode 74. Search a 2D Matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> >& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int start=0;
+        int end=row*col-1;
+        int mid=start+ (end-start)/2;
+
+        while(mid<=end){
+            int element=matrix[mid/col][mid%col];
+
+            if(target==element){
+                return true;
+            }
+
+            if(element<target){
+                start=mid+1;
+            }
+
+            if(element>target){
+                end=mid-1;
+            }
+
+            mid=start+(end-start)/2;
+        }
+
+        return false;
+        
+    }
+};
