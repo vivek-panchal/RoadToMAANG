454d26890938c075074aaa1d51e173cf137ee7ae
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-07-15 09:08:15 +0530

    Create Leetcode 496. Next Greater Element I.cpp

---
+++ Leetcode 496. Next Greater Element I.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums2.size();
+        int m=nums1.size();
+        unordered_map<int,int> umap;
+        stack<int> st;
+        //nums 2 ke next greater element ko map me store kar lenge nums2[i] -> next greater element
+        for(int i=n-1; i>=0;i--){
+            int ele=nums2[i];
+            while(!st.empty()&& st.top()<=ele){
+                st.pop();
+            }
+            //ager stack empty hai to next greater element -1 hoga verna jo stack ke top per hai whi hoga
+            int res=(st.empty()) ? -1 : st.top();
+            umap.insert({ele,res});
+            st.push(ele);
+        }
+        
+        // nums1 ko traverse karenge and nums[i] ko map me dhundhenge and uske samne jo bhi value hai usse store kar lenge ans me 
+        vector<int> ans;
+        for(int i=0;i<m;i++){
+            int x= nums1[i];
+            ans.push_back(umap[x]);
+        }
+        return ans;
+    }
+};
